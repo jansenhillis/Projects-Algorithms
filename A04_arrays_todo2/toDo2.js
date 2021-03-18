e3fbbe225c2d5433512a7279ba28f2ece7b6c84b
Jansen Hillis <jansen.hillis@outlook.com>
2021-03-17 20:57:28 -0700

    Added two new rotates

--- A04_arrays_todo2/toDo2.js
+++ A04_arrays_todo2/toDo2.js
@@ -24,20 +24,64 @@ function reverse(arr) {
 // Implement rotateArr(arr, shiftBy) that accepts array and offset. Shift arr’s values to the 
 // right by that amount. ‘Wrap-around’ any values that shift off array’s end to the other side, so that 
 // no data is lost. Operate in-place: given ([1,2,3],1), change the array to [3,1,2]. Don’t use built-in functions.
+function rotate(arr, shiftBy) {
+    if (typeof arr !== 'undefined') {
 
+        while (shiftBy > 0) {
+            var temp = arr[arr.length-1];
+            for (var i = arr.length-1; i > 0; i--) {
+                arr[i] = arr[i-1];
+            }
+            arr[0] = temp;
+            shiftBy--;
+        }
+        return arr;
+    }
+    return null;
+}
 
 
 // Second: allow negative shiftBy (shift L, not R).
+function rotateLR(arr, shiftBy) {
+    if (typeof arr !== 'undefined') {
+
+        while (shiftBy != 0) {
+            var temp = 0;
+            
+            // Shift right
+            if (shiftBy > 0) {
+                temp = arr[arr.length-1];
+                for (var i = arr.length-1; i > 0; i--) {
+                    arr[i] = arr[i-1];
+                }
+                arr[0] = temp;
+                shiftBy--;
+            } else if (shiftBy < 0) { // Shift left
+                temp = arr[0];
+                for (var i = 0; i < arr.length-1; i++) {
+                    arr[i] = arr[i+1];
+                }
+                arr[arr.length-1] = temp;
+                shiftBy++;
+            } else {
+                break;
+            }
+        }
+        return arr;
+    }
+    return null;
+}
 
 // Third: minimize memory usage. With no new array, handle arrays/shiftBys in the millions.
-
 // Fourth: minimize the touches of each element.
 
 // Filter Range
 // Alan is good at breaking secret codes. One method is to eliminate values that lie within a specific known range. 
 // Given arr and values min and max, retain only the array values between min and max. 
 // Work in-place: return the array you are given, with values in original order. No built-in array functions.
+function filterRange(arr, min, max) {
 
+}
 
 
 // Concat
