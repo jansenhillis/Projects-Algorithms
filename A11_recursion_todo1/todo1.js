04fc3224645836a7ceaf7494c45a5d77d629bb34
Jansen Hillis <jansen.hillis@outlook.com>
2021-03-27 11:45:26 -0700

    save

--- A11_recursion_todo1/todo1.js
+++ A11_recursion_todo1/todo1.js
@@ -29,3 +29,23 @@ function rFact(value) {
     }
     return value * rFact(value - 1);
 }
+
+
+// Optional
+// Flood Fill 
+// floodFill(2Darray, [2,2], 1)          
+// [[3,2,3,4,3],       [[3,2,1,4,3],
+//  [2,3,3,4,9],        [2,1,1,4,0],
+//  [7,3,3,5,3],  ==>   [7,1,1,5,3],
+//  [6,5,3,4,1],        [6,5,1,4,1],
+//  [1,2,3,3,3]]        [1,2,1,1,1]]
+
+function floodFill(canvas2D, startXY, newColorID) {
+    var numRows = canvas2D.length;
+    var numCols = canvas2D[0].length;
+    var x = startXY[0];
+    var y = startXY[1];
+
+    // base case: no adjacent cells with matching value at [x,Y]
+    
+}
